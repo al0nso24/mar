fabbdfbae920f506487dc3f4f91b65f4f864b965
Alonso Zegarra Velasquez <zegarrav24@gmail.com>
2026-02-22 19:27:19 -0500

    Tercer commit

--- src/Footer.js
+++ src/Footer.js
@@ -1,30 +1,33 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-    return (
-        <footer class="page-footer grey darken-4">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">OceanDepths</h5>
-                <p class="grey-text text-lighten-4">Explorando la vida marina desde la superficie hasta el abismo.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Secciones</h5>
-                <ul>
-                  <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/tiburones">Tiburones</Link></li>
-                  <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/cetaceos">Cetáceos</Link></li>
-                  <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/abisal">Fauna abisal</Link></li>
-                  <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/corales">Corales</Link></li>
-                </ul>
-              </div>
-            </div>
+  return (
+    <footer class="page-footer grey darken-4">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">OceanDepths</h5>
+            <p class="grey-text text-lighten-4">Explorando la vida marina desde la superficie hasta el abismo.</p>
           </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2026 Copyright Text
-            </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Secciones</h5>
+            <ul>
+              <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/tiburones">Tiburones</Link></li>
+              <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/cetaceos">Cetáceos</Link></li>
+              <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/abisal">Fauna abisal</Link></li>
+              <li className="enlaces_footer"><Link class="grey-text text-lighten-3" to="/corales">Corales</Link></li>
+            </ul>
           </div>
-        </footer>
-    )
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+          © 2026 Alonso Zegarra Velásquez
+        </div>
+      </div>
+      <div class="container" style={{ display: "flex", alignItems: "center", gap: "8px", padding: "10px 0" }}>
+        <i class="material-icons">mail</i> zegarrav24@gmail.com
+      </div>
+    </footer>
+  )
 }
